767f6b3547481aa22124e4e3f388c269e6b61ee8
hita-fttk <fttkmcc77@gmail.com>
2024-02-21 09:49:33 +0900

    higaki　1-1と1‐2のコミット

---
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -0,0 +1,54 @@
+package com.example.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * 管理者情報登録時に使用するフォーム.
+ * 
+ * @author igamasayuki
+ * 
+ */
+public class InsertAdministratorForm {
+	/** 名前 */
+	@NotBlank(message = "名前を入力してください")
+	private String name;
+	/** メールアドレス */
+	@Email(message = "メールアドレスの形式で入力してください")
+	private String mailAddress;
+	/** パスワード */
+	@Size(min = 8, max = 16, message = "8文字以上16文字以内で入力してください")
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+				+ "]";
+	}
+
+}
---
+++ src/main/java/com/example/form/LoginForm.java
@@ -0,0 +1,43 @@
+package com.example.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ * ログイン時に使用するフォーム.
+ * 
+ * @author igamasayuki
+ * 
+ */
+public class LoginForm {
+
+	/** メールアドレス */
+	@Email(message = "メールアドレスの形式で入力してください")
+	private String mailAddress;
+	/** パスワード */
+	@Size(min = 8, max = 16, message = "8文字以上16文字以内で入力してください")
+	private String password;
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+	}
+
+}
